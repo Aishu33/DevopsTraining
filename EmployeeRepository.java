9f69319ab2beca76ba601d658b2be18d30060beb
Aishu33 <46428724+Aishu33@users.noreply.github.com>
2020-04-08 11:14:01 -0400

    Add files via upload

---
+++ EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.db.springJPAApplication;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Integer>
+{
+
+}
